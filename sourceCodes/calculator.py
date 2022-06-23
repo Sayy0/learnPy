23876d29c22b3e8112f8e1dc5d35b79ff16aa47f
Say0 <jiantan420@yahoo.com>
2022-06-24 07:18:42 +0800

    Add while to prevent invalid operation selection, add exception handler for NaN

--- sourceCodes/calculator.py
+++ sourceCodes/calculator.py
@@ -1,3 +1,5 @@
+import math
+
 print("\n=========================")
 print("\n (0) Cancel Operation)")
 print("\n (1) + ")
@@ -5,28 +7,34 @@ print("\n (2) - ")
 print("\n (3) x ")
 print("\n (4) / ")
 
-operation = str(input("\nChoose Operation : "))
+operation = -1
 
-if operation == "0":
-    print("Cancelled Operation, exiting program")
+while operation < 0 or operation > 4 or math.isnan(operation):
+    try:
+        operation = int(input("\nChoose Operation : "))
+    except:
+        print("\nPlease enter a valid selection!!\n")
+
+if operation == 0:
+    print("\nCancelled Operation, exiting program.\n")
     exit()
+else:
+    num1 = int(input("Num 1 : "))
+    num2 = int(input("Num 2 : "))
 
-num1 = int(input("Num 1 : "))
-num2 = int(input("Num 2 : "))
+    match operation:
+        case '1':
+            answer = num1+num2
+            symbol = '+'
+        case '2' :
+            answer = num1-num2
+            symbol = '-'
+        case '3' :
+            answer = num1*num2
+            symbol = 'x'
+        case '4' :
+            answer = num1/num2
+            symbol = '/'
 
-match operation:
-    case '1':
-        answer = num1+num2
-        symbol = '+'
-    case '2' :
-        answer = num1-num2
-        symbol = '-'
-    case '3' :
-        answer = num1*num2
-        symbol = 'x'
-    case '4' :
-        answer = num1/num2
-        symbol = '/'
-
-print("\n=======================================")
-print("Answer: " , num1, " ", symbol, " ", num2, " = ", answer)
+    print("\n=======================================")
+    print("Answer: " , num1, " ", symbol, " ", num2, " = ", answer)
